00ca52d5650330bdfc60c66d9f3549602ae8a5e9
codinghunter-source <rohitkumawat6367@gmail.com>
2026-06-06 00:29:57 +0530

    Fix Clarity script

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -21,15 +21,15 @@ export default function RootLayout({
         {children}
 
         <Script id="clarity" strategy="afterInteractive">
-          {`
-            (function(c,l,a,r,i,t,y){
-                c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
-                t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
-                y=l.getElementsByTagName(r)[0];
-                y.parentNode.insertBefore(t,y);
-            })(window, document, "clarity", "script", "x2ewt3jbg9");
-          `}
-        </Script>
+  {`
+    (function(c,l,a,r,i,t,y){
+        c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+        t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+        y=l.getElementsByTagName(r)[0];
+        y.parentNode.insertBefore(t,y);
+    })(window, document, "clarity", "script", "x2ewt3jbg9");
+  `}
+</Script>
       </body>
     </html>
   );
